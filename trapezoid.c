e7f785a59bc18eef67db12555df269d7f36979bb
sullihuk <nyxa@nextmail.ru>
2023-03-07 05:03:30 -0500

    Some comments have added

--- trapezoid.c
+++ trapezoid.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#define degToRad(degreesAngle) ((degreesAngle) * M_PI / 180)
+#define degToRad(degreesAngle) ((degreesAngle) * M_PI / 180) //Т.к. язык С вычисляет тригонометрические функции от значений выраженных в радианах, определяется функцию конвертации градусов в радианы, ввиду того, что в российской образовательной традиции стоящее под тригонометрической функцией число обычно выражено в градусах, если иное не указано специально 
 
-float trapezoidMethod(float a, float b, int n)
+float trapezoidMethod(float start, float finish, int n);//Инициализация метода трапеций
+
+float trapezoidMethod(float start, float finish, int n)//Функция реализует метод трапеций для вычисления определенного интеграла
 {
-	float h, x, s;
-	int i;
+	float h, x, s;//Инициализация переменных 
 	
 	float equality(float param)
 	{
@@ -15,14 +16,14 @@ float trapezoidMethod(float a, float b, int n)
 		return equ;
 	}
 	
-	s=(equality(a)+equality(b))/2;
+	s=(equality(start)+equality(finish))/2;
 	
-	h=(b-a)/n;
+	h=(finish-start)/n;
 	x=a+h;
-	for (i=1; i<=n-1; i++)
+	for (int i=1; i<=n-1; i++)
 	{
-		s=s+equality(x);
-		x=x+h;
+		s += equality(x);
+		x += h;
 	}
 	return s*h;
 }
